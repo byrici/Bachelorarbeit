ea7e36571487890ede62cb2234fcd0247a9b8180
rosburg <r.osburg@uni-muenster.de>
2025-08-19 09:49:52 +0000

    Test

--- src/behavior_tree/nodes/walk_backwards.py
+++ src/behavior_tree/nodes/walk_backwards.py
@@ -11,7 +11,7 @@ def move_forward(x_stride=-0.06, num_cycles=1, gait_type='tripod'):
     bot.hex.move_in_world(x_stride = x_stride, num_cycles = num_cycles, gait_type = gait_type)
     time.sleep(0.04)
     
-    #bot.hex.reset_hexapod('sleep')
+   
 
 if __name__=='__main__':
     move_forward()
