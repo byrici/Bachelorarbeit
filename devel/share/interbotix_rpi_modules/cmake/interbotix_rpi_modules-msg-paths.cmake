c5a1f3eb41943cebbcc99a134a98d4ed980abc2c
Ricardo Osburg <r.osburg@uni-muenster.de>
2025-09-17 11:29:55 +0200

    Delete devel directory

--- devel/share/interbotix_rpi_modules/cmake/interbotix_rpi_modules-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(interbotix_rpi_modules_MSG_INCLUDE_DIRS "/my_project/src/interbotix_ros_toolboxes/interbotix_rpi_toolbox/interbotix_rpi_modules/msg")
-set(interbotix_rpi_modules_MSG_DEPENDENCIES std_msgs)
